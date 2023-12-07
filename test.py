cd9fd878d5dac8960a8d212648ae5a59893903c7
Myong <johun04@kakao.com>
2023-12-07 13:08:09 +0900

    Add hand shape and simple click
    (o) is possible now , (x) is not possible yet
    left click (o) / double left click (o) / long left click (x) / right click (o) / scroll up (x) / scroll down (x)

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 import time
+import mouse
 
 
 def get_finger_landmarks(hand_landmarks, finger_indices):
@@ -26,6 +27,15 @@ prev_frame_time = 0
 # 지금 프레임 시간
 new_frame_time = 0
 
+#더블클릭 체크용 변수
+left = 0
+firtime = 0
+sectime = 0
+clicked = False
+
+#클릭 유지 체크용 변수
+longcheck = 0
+
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
@@ -44,7 +54,6 @@ while cap.isOpened():
     # MediaPipe로 손 인식
     results = hands.process(frame_rgb)
 
-    
     if results.multi_hand_landmarks:
         # 손 랜드마크 그리기
         for hand_landmarks in results.multi_hand_landmarks:
@@ -70,6 +79,8 @@ while cap.isOpened():
                                       mp_hands.HandLandmark.PINKY_DIP,
                                       mp_hands.HandLandmark.PINKY_PIP,
                                       mp_hands.HandLandmark.PINKY_MCP]
+            WRIST_LANDMARK = [mp_hands.HandLandmark.WRIST]
+            
 
             # 엄지손가락 랜드마크
             thumb_landmarks = get_finger_landmarks(hand_landmarks, THUMB_LANDMARKS)
@@ -81,14 +92,82 @@ while cap.isOpened():
             ring_finger_landmarks = get_finger_landmarks(hand_landmarks, RING_FINGER_LANDMARKS)
             # 새끼손가락 랜드마크
             pinky_finger_landmarks = get_finger_landmarks(hand_landmarks, PINKY_FINGER_LANDMARKS)
+            #손목 랜드마크
+            wrist_landmark = get_finger_landmarks(hand_landmarks, WRIST_LANDMARK)[0]
 
 
-            distance = calculate_distance(thumb_landmarks[0], index_finger_landmarks[0]);
-            if distance < 0.1:
-                print("clicked")
+            #거리
+            idx_thb = calculate_distance(thumb_landmarks[0], index_finger_landmarks[0])             #검지 1번 엄지 1번
+            mid_thb = calculate_distance(thumb_landmarks[0], middle_finger_landmarks[0])            #중지 1번 엄지 1번
+            thb_grip = calculate_distance(thumb_landmarks[0],middle_finger_landmarks[3])
+            fold_pinky = calculate_distance(pinky_finger_landmarks[2],pinky_finger_landmarks[3])    #새끼 3번, 4번마디
+            
+
+            grip_idx = calculate_distance(index_finger_landmarks[1],index_finger_landmarks[3])
+
+
+            #print(thb_grip) 0.05
+
+            if grip_idx < 0.1 and fold_pinky < 0.03:
+                time.sleep(0.1)
+                idx_thb = 1
+
+                if thb_grip < 0.05:
+                    print("Scroll Down")
+
+                elif thb_grip > 0.1:
+                    print("Scroll Up")
+
+            elif idx_thb < 0.1:
+                time.sleep(0.05)
+
+                if mid_thb < 0.1:
+                    print("Right Click")
+                    mouse.right_click()
+                    time.sleep(0.2)
+
+                else:
+                    if clicked:
+                        longcheck += 1
+                        if longcheck >= 5:
+                            print("Long Left Click")
+
+
+
+
+                    if not clicked:  
+                        clicked = True
+                        current_time = time.perf_counter()
+                        if left == 0:
+                            firtime = current_time
+                            left += 1
+                            print("Left Click")
+                            mouse.click()
+                        elif left == 1:
+                            if current_time - firtime < 0.5:
+                                print("Double Click")
+                                mouse.double_click()
+                                left = 0  
+                            else:
+                                firtime = current_time  
+                                print("Left Click")
             else:
+                longcheck = 0
+                clicked = False  
                 print("none")
 
+
+            #________________________________________________________________
+            wristx = 3412 - wrist_landmark.x * 3412
+            wristy = wrist_landmark.y * 3099 - 1599
+            wristx = wristx / 2
+            wristy = wristy
+
+            mouse.move(wristx, wristy)
+
+
+             
+                
 
     # FPS 계산
     fps = 1 / (new_frame_time - prev_frame_time)
@@ -103,6 +182,9 @@ while cap.isOpened():
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+    
+
 # 자원 해제
 cap.release()
 cv2.destroyAllWindows()
+
